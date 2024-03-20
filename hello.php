910779f06d5fa18178bcff1f078da156266fc74a
saraalyan <alyansara817@gmail.com>
2024-03-21 01:01:33 +0400

    second

--- hello.php
+++ hello.php
@@ -3,8 +3,11 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="bootstrap.css">
     <title>Document</title>
 </head>
 <body>
-<h2>sadasdfasd</body>
+    <h1>hello my php</h1>
+</body>
+<script src="bootstrap.js"></script>
 </html>
