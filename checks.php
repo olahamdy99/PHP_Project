1131d25f831a8008c4b158b425d694d44c7c98f1
gehad <gehadmarawan7@gmail.com>
2024-03-22 12:09:15 +0200

    First

---
+++ checks.php
@@ -0,0 +1,108 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+    <div class="container">
+        <h2>Add Product....</h2>
+        <div class="row">
+            <br>
+            <div class="col-sm-12" style="height:75px;">
+                <div class="" ">
+                    <input type="date" id="from" name="from">
+                    <label for="from">Date From</label>
+                </div>
+<br>
+                <div>
+                    <input type="date" id="to" name="to">
+
+                    <label for="to">Date To</label>
+                    <div class="form-group">
+                        <label for="btn"></label>
+                        <select class="form-control" id="btn">
+                            <option>User</option>
+                            <option>Admin</option>
+                        </select>
+                    </div>
+                </div>
+                
+              
+                    <br>
+
+                    <div class="col-sm-12" style="height:75px;">
+                        <table class="table">
+                            <thead>
+                              <tr>
+                               
+                                <th scope="col">Order Date</th>
+                                <th scope="col">Amount</th>
+                              </tr>
+                            </thead>
+                            <tbody>
+                              <tr>
+                                <th scope="row">1</th>
+                             
+                                <td>@mdo</td>
+                              </tr>
+                              <tr>
+                                <th scope="row">2</th>
+                           
+                                <td>@fat</td>
+                              </tr>
+                              <tr>
+                                <th scope="row">3</th>
+                                <td>@twitter</td>
+                              </tr>
+                            </tbody>
+                          </table>   
+                    </div>
+<br>
+<br>
+<br>
+
+<br>
+
+     
+<div class="col-sm-12" style="height:75px;">
+    <table class="table">
+        <thead>
+          <tr>
+           
+            <th scope="col">Order Date</th>
+            <th scope="col">Amount</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th scope="row">1</th>
+         
+            <td>@mdo</td>
+          </tr>
+          <tr>
+            <th scope="row">2</th>
+       
+            <td>@fat</td>
+          </tr>
+          <tr>
+            <th scope="row">3</th>
+            <td>@twitter</td>
+          </tr>
+        </tbody>
+      </table>   
+</div>
+
+<script src="assets/js/bootstrap.bundle.min.js"></script>
+<script src="assets/js/main.js"></script>
+
+<script>
+   
+</script>
+
+</body>
+</html>
