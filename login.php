afb502ab7ffa722ad2af517bc3e8bb0160c1899f
saraalyan <alyansara817@gmail.com>
2024-03-26 08:44:52 +0400

    login

--- login.php
+++ login.php
@@ -1,43 +1,31 @@
 <?php
 require("db.php");
 
-// Instantiate the database connection
 $db = new db();
 
-// Check if the form is submitted
 if ($_SERVER["REQUEST_METHOD"] === "POST") {
     try {
-        // Get the email and password from the form
         $email = $_POST['email'];
         $password = $_POST['Password'];
 
-        // Fetch user data from the database based on the provided email
         $user = $db->get_data('users', "*", "`email`='$email'");
 
-        // If user data is found
         if ($user && $user->rowCount() > 0) {
-            // Fetch user data as an associative array
             $userData = $user->fetch(PDO::FETCH_ASSOC);
-            // Get the hashed password from the user data
             $hashedPassword = $userData['hash_password'];
 
-            // Verify the provided password against the hashed password
             if (password_verify($password, $hashedPassword)) {
-                // Redirect to success page if login is successful
                 header("Location: success.html");
                 exit;
             } else {
-                // Redirect to login page with error message if password is incorrect
                 header("Location: login.php?error=1");
                 exit;
             }
         } else {
-            // Redirect to login page with error message if user does not exist
             header("Location: login.php?error=1");
             exit;
         }
     } catch (PDOException $e) {
-        // Handle database errors
         echo $e->getMessage();
     }
 }
